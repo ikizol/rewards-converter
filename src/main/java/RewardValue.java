1e33d0312d5838e2c0eac07bbf0a02796275a267
Genesis <genesis.castillo52@myhunter.cuny.edu>
2024-09-14 22:43:50 -0400

    completed reward class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class RewardValue {
-}
+    private
+    double cashValue;
+    int milesValue;
+    static double milesTocash_ ;
+
+
+    public RewardValue (double cashValue) {
+         this.cashValue= cashValue;
+         this.milesValue = (int) (cashValue / milesTocash_);
+    };
+    public RewardValue(int milesValue_) {
+            this.milesValue = milesValue;
+            this.cashValue = milesTocash_ + milesValue;
+
+    };
+
+    public double getCashValue() {
+        return cashValue;
+    };
+
+    public int getMilesValue() {
+        return milesValue;
+    };
+
+
+
+};
